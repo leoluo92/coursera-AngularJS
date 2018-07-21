e43787e593aadd0043cc372ee31dbc1ab514c47d
leoluo <leoluo92@gmail.com>
2018-07-21 12:47:00 -0500

    added color and border color

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,7 +9,36 @@ LunchCheckController.$inject=['$scope'];
 function LunchCheckController( $scope ){
   $scope.msgPlaceHolder="list comma separated dishes you usually have for lunch";
   $scope.lunchMenu="";
-  $scope.name="leoluo";
+  $scope.finalresult="";
+
+  $scope.checkIfTooMuch= function (){
+    var num = $scope.lunchMenu.split(",").length;
+    if ($scope.lunchMenu==""){
+      $scope.finalresult= "Please enter data first";
+    }
+    else if(num>3){
+      $scope.finalresult="Too much!";
+    }
+    else {
+      $scope.finalresult="Enjoy!";
+    }
+  };
+
+  $scope.finalresultStyle=function(){
+    if ($scope.finalresult=="Please enter data first"){
+      return {"color": "red"};
+    }
+    else return {"color":"green"};
+  }
+
+  $scope.lunchMenuStyle=function(){
+    if ($scope.finalresult=="") return;
+    if ($scope.finalresult=="Please enter data first"){
+      return {"border-color": "red"};
+    }
+    else return {"border-color":"green"};
+  }
+
 };
 
 
